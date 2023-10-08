6bae1c31d2b95fbd83c76569f6a2d2fb1562acc0
kalininasofa <143995240+kalininasofa@users.noreply.github.com>
2023-10-08 13:34:29 +0300

    Update script.js

--- laba5/script.js
+++ laba5/script.js
@@ -1,20 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
 var itogo = document.getElementById("itogo");
 itogo.addEventListener("click", kalkuliator);
 });
+
+function validate() {
+var count = document.getElementById("count").value;
+var o = /^[\d]$/;
+var valid = o.test(count);
+
+if(valid) output = "Данные верны";
+else output = "Неверные данные";
+document.getElementById("message").innerHTML = output;
+return valid;
+}
 
 function kalkuliator() {
 var name = document.getElementById("select");
 var count = document.getElementById("count");
 var result = document.getElementById("result");
 
-if(count.value == "") alert("Введите количество товаров");
-else {
+if(validate()){
 var pr = 0;
 pr += parseInt(name.options[name.selectedIndex].value);
 pr = parseInt(count.value) * pr;
-
 result.innerHTML = pr;
 }
+else alert("Не считается");
 }
-
