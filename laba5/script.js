f3a46b88a7b636c84fd39d4b5ff871a3e505b1d2
kalininasofa <143995240+kalininasofa@users.noreply.github.com>
2023-10-09 13:02:25 +0300

    Update script.js

--- laba5/script.js
+++ laba5/script.js
@@ -1,29 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 var itogo = document.getElementById("itogo");
-itogo.addEventListener("click", kalkuliator);
+itogo.addEventListener("click", kalkuliator());
 });
 
-function validate() {
-var count = document.getElementById("count").value;
-var o = /^[\d]$/;
-var valid = o.test(count);
-
-if(valid) output = "Данные верны";
-else output = "Неверные данные";
-document.getElementById("message").innerHTML = output;
-return valid;
-}
-
-function kalkuliator() {
+function kalkuliator(){
 var name = document.getElementById("select");
 var count = document.getElementById("count");
 var result = document.getElementById("result");
 
-if(validate()){
-var pr = 0;
-pr += parseInt(name.options[name.selectedIndex].value);
-pr = parseInt(count.value) * pr;
-result.innerHTML = pr;
+if(count.value == "") alert("Введите количество товаров:");
+else {
+var price = 0;
+price += parseInt(name.options[name.selectedIndex].value);
+price = parseInt(count.value) * price;
+
+result.innerHTML = price;
 }
-else alert("Не считается");
 }
