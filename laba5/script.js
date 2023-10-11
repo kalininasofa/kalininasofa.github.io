2b8356b5910ae0cd5e45da32ec76db2ec104f846
kalininasofa <143995240+kalininasofa@users.noreply.github.com>
2023-10-11 10:36:48 +0300

    Update script.js

--- laba5/script.js
+++ laba5/script.js
@@ -8,12 +8,11 @@ var name = document.getElementById("select");
 var count = document.getElementById("count");
 var result = document.getElementById("result");
 
-if(count.value == "") alert("Введите количество товаров:");
+if(count.value == "" && count.value < 0) alert("Введите необходимые данные:");
 else {
 var price = 0;
 price += parseInt(name.options[name.selectedIndex].value);
 price = parseInt(count.value) * price;
-
 result.innerHTML = price;
 }
 }
