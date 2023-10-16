617eab05810222cb67ffe842065de89ac259dd9b
kalininasofa <143995240+kalininasofa@users.noreply.github.com>
2023-10-16 10:01:52 +0300

    Update script.js

--- laba6/script.js
+++ laba6/script.js
@@ -1,29 +1,21 @@
 var k;
-
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener('DOMContentLoaded', function(event) {
   let l = document.getElementById("myselect");
   k = 0;
   l.addEventListener("change", function(event) {
     let select = event.target;
-    let v = document.getElementById("my select");
+    let v = document.getElementById("select1");
     let check = document.getElementById("checkboxes");
     let radios = document.getElementById("myradios");
     let count = document.getElementById("count");
-            
-    if (select.value == "0") {
-      v.style.display = "none";
-      check.style.display = "none";
-      radios.style.display = "none";
-      count.style.display = "none";
-    }
     if (select.value == "1") {
-      v.style.display = "none";
+      v.style.display = "block";
       check.style.display = "none";
       radios.style.display = "block";
       count.style.display = "block";
     }
     if (select.value == "2") {
-      v.style.display = "block";
+      v.style.display = "none";
       check.style.display = "none";
       radios.style.display = "block";
       count.style.display = "block";
@@ -36,7 +28,6 @@ window.addEventListener('DOMContentLoaded', function (event) {
     }
     k = parseInt(select.value);
   });
-  
   let br = document.querySelectorAll(".checkboxes input[type=checkbox]");
   br.forEach(function(checkbox) {
     checkbox.addEventListener("change", function(event) {
@@ -45,16 +36,15 @@ window.addEventListener('DOMContentLoaded', function (event) {
     });
   });
 });
-    
+
 function validate() {
   var count = document.getElementById("count").value;
   var o = /^\d{1,}$/;
   var valid = o.test(count);
-
   if (valid) {
-    window.output = "Данные верны";
+    window.output = "верно";
   } else {
-    window.output = "Неверные данные";
+    window.output = " неверно! ";
   }
   document.getElementById("message").innerHTML = window.output;
   return valid;
@@ -62,90 +52,121 @@ function validate() {
 
 function kalkuliator() {
   var result = document.getElementById("result");
-  var m = k;
+  var t = k;
   var res;
-  console.log(m);
-  
-  if(validate()) {
-    if(k == 1) {
-      res = kalkuliatorsertif();
+  console.log(t);
+  if (validate()) {
+    if (k == 1) {
+      res = kalkuliatefleshka();
       result.innerHTML = res;
     }
-    if(k == 2) {
-      res = kalkuliatorigr();
+    if (k == 2) {
+      res = kalkuliatecards();
       result.innerHTML = res;
     }
-    if(k == 3) {
-      res = kalkuliatorflow();
+    if (k == 3) {
+      res = kalkuliatepods();
       result.innerHTML = res;
     }
-  }
-  else {
-    window.alert("oo");
+  } else {
+    window.alert("!");
   }
 }
 
-function kalkuliatorsertif(){
+function kalkuliatefleshka() {
   var count = document.getElementById("count");
   var d = document.getElementsByName("myradio");
-
-  var f = 0;
-  dostavka.forEach(e => {
-    if (e.checked) {
-      f = e.value;
-    }
-  });
+  var fleshka = document.getElementById("select1");
   var price = 0;
-  var stoimost = 5000;
-
-  price += (parseInt(stoimost) * parseInt(count.value) + parseInt(f));
-
-  return price;
-}
-
-function kalkuliator(){
-  var count = document.getElementById("count");
-  var d = document.getElementsByName("myradio");
-  var flesh = document.getElementById("select1");
-  var price = 0;
-  var f;
+  var oo = 0;
   d.forEach(e => {
     if (e.checked) {
-      f = e.value;
+      oo = e.value;
     }
   });
-
-  price += parseInt(flesh.options[flesh.selectedIndex].value);
+  price += parseInt(fleshka.options[fleshka.selectedIndex].value);
   console.log(price);
   price = parseInt(count.value) * price;
   console.log(price);
-  price += parseInt(f);
+  price += parseInt(oo);
   console.log(price);
-
   return price;
 }
 
-function kalkuliatorflow(){
+function kalkuliatecards() {
   var count = document.getElementById("count");
-  var d= document.getElementsByName("myradio");
-  var o1 = document.getElementsByName("props");
-
-  var f = 0;
+  var d = document.getElementsByName("myradio");
+  var oo = 0;
   d.forEach(e => {
     if (e.checked) {
-      f = parseInt(e.value);
+      oo = e.value;
     }
   });
-  o1.forEach(e => {
-    if (e.checked) {
-      f += parseInt(e.value);
-    }
-  });
-
   var price = 0;
-  var stoimost = 3000;
+  var pr = 5000;
+  price += (parseInt(pr) * parseInt(count.value) + parseInt(oo));
+  return price;
+}
 
-  price += ((stoimost * parseInt(count.value)) + f);
+function kalkuliatepods() {
+  var count = document.getElementById("count");
+  var d = document.getElementsByName("myradio");
+  var option = document.getElementsByName("props");
+  var oo = 0;
+  d.forEach(e => {
+    if (e.checked) {
+      oo = parseInt(e.value);
+    }
+  });
+  option.forEach(e => {
+    if (e.checked) {
+      oo += parseInt(e.value);
+    }
+  });
+  var price = 0;
+  var pr = 4000;
+  price += ((pr * parseInt(count.value)) + oo);
+  return price;
+}
 
+function kalkuliateperehodnik() {
+  var count = document.getElementById("count");
+  var d = document.getElementsByName("myradio");
+  var option = document.getElementsByName("props");
+  var oo = 0;
+  d.forEach(e => {
+    if (e.checked) {
+      oo = parseInt(e.value);
+    }
+  });
+  option.forEach(e => {
+    if (e.checked) {
+      oo += parseInt(e.value);
+    }
+  });
+  var price = 0;
+  var pr = 3500;
+  price += ((pr * parseInt(count.value)) + oo);
+  return price;
+}
+
+function kalkuliatechehol() {
+  var count = document.getElementById("count");
+  var d = document.getElementsByName("myradio");
+  var option = document.getElementsByName("props");
+  var oo = 0;
+  d.forEach(e => {
+    if (e.checked) {
+      oo = parseInt(e.value);
+    }
+  });
+  option.forEach(e => {
+    if (e.checked) {
+      oo += parseInt(e.value);
+    }
+  });
+  var price = 0;
+  var pr = 3000;
+  price += ((pr * parseInt(count.value)) + oo);
   return price;
 }
